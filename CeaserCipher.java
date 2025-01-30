6290d6f01827cc6127bb2db5efb83acf948e7352
AbhayKrishnaDubey <abhaykrishnadubey@gmail.com>
2025-01-30 23:45:08 +0530

    added the files hill cipher and playfair

--- CeaserCipher.java
+++ CeaserCipher.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LAB_01_ceaserCipherJava {
+public class CeaserCipher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
